3d60218e5d38e7d7c4e9052c9e72cc6dae972d95
huyuyh <phamhuy09042002@gmail.com>
2026-01-23 17:03:33 -0500

    add proxy

---
+++ proxy.ts
@@ -0,0 +1,17 @@
+import { getSession } from "@/lib/auth/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+const proxy = async (request: NextRequest) => {
+  const session = await getSession();
+
+  const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+};
+
+export default proxy;
